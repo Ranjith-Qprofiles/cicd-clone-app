66d5a1079d407d17415dfccc39bd376b69a9a3ce
ranjith <ranjith@gmail.com>
2023-12-20 18:45:35 +0530

    Side Bar Font Changed from blue to red color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+iimport React from "react";
 import { Stack } from "@mui/material";
 
 import { categories } from "../utils/constants";
@@ -22,7 +22,7 @@ const Categories = ({ selectedCategory, setSelectedCategory }) => (
         }}
         key={category.name}
       >
-        <span style={{ color: category.name === selectedCategory ? "white" : "blue", marginRight: "15px" }}>
+        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
           {category.icon}
         </span>
         <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
